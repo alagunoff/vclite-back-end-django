79b1f84cbc5a6568f0687b0ca611482c57464047
Artem Lagunov <lagunov.artamon@gmail.com>
2022-09-20 11:18:32 +0300

    used related name to get access to subcategories

--- src/blog/utils.py
+++ src/blog/utils.py
@@ -1,7 +1,5 @@
 from typing import Any
 
-from .models.category import Category
-
 
 def map_tag_to_dict(tag: Any) -> dict[str, int | str]:
     return {
@@ -14,5 +12,5 @@ def map_category_to_dict(category: Any) -> dict[str, Any]:
     return {
         'id': category.id,
         'category': category.category,
-        'subcategories': list(map(map_category_to_dict, Category.objects.filter(parent_category=category)))
+        'subcategories': list(map(map_category_to_dict, category.subcategories.all()))
     }
