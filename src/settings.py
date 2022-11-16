d0cf76cacf3ff3628b5877d778449578d867997f
Artem Lagunov <lagunov.artamon@gmail.com>
2022-11-16 09:49:12 +0300

    removed secret key from settings

--- src/settings.py
+++ src/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2$m7*79fx-*^9k6f63xa1!_sl$$ex$_vdso*0l9p8=5t30lqr+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'true'
